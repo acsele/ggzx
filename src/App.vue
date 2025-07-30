88fd839ffd9195f7b40d729a8b4cc11298fe7dce
xingchen <1906616510@qq.com>
2025-07-30 17:07:59 +0800

    test husky

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,16 @@
 </template>
 
 <script setup lang="ts">
-const a = 0
+  const a = 0
 const str = 'hi'
+function sss()
+
+
+{
+        const a = 1;
+  return a + 1;
+
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
